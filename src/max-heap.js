b3d4f63cc9486dcac09c2c7efa352bf42871f407
Andrew <andrew-6676@ya.ru>
2019-03-24 22:24:52 +0300

    complete

--- src/max-heap.js
+++ src/max-heap.js
@@ -2,47 +2,150 @@ const Node = require('./node');
 
 class MaxHeap {
 	constructor() {
-		
+        this.root = null;
+        // массив свободных для добавления родителей
+        this.parentNodes = [];
 	}
 
 	push(data, priority) {
-		
+        const node = new Node(data, priority);
+        this.insertNode(node);
+        this.shiftNodeUp(node);
 	}
 
 	pop() {
-		
+        if (!this.isEmpty()) {
+            const detached = this.detachRoot();
+            this.restoreRootFromLastInsertedNode(detached);
+            this.shiftNodeDown(this.root);
+            return detached.data;
+        }
 	}
 
 	detachRoot() {
-		
+        const root = this.root;
+        if (~this.parentNodes.indexOf(root)) {
+            this.parentNodes.shift()
+        }
+        this.root = null;
+        return root;
 	}
 
 	restoreRootFromLastInsertedNode(detached) {
-		
+        if (!this.isEmpty() || this.parentNodes.length != 0) {
+            const last = this.parentNodes.pop();
+            if (!last.parent) {
+                return this.root = last;
+            }
+            if (last.parent.right === last && last.parent !== detached) {
+                this.parentNodes.unshift(last.parent);
+            }
+            last.remove();
+            if (detached !== last.parent) {
+                if (detached.left) {
+                    last.appendChild(detached.left);
+                }
+                if (detached.right) {
+                    last.appendChild(detached.right);
+                }
+            }
+            if (!last.right) {
+                this.parentNodes.unshift(last);
+            }
+            this.root = last;
+        }
 	}
 
 	size() {
-		
+	    function count(node) {
+	        if (node) {
+                return count(node.left) + count(node.right)+1;
+            } else {
+	            return 0;
+            }
+        }
+        return count(this.root);
 	}
 
 	isEmpty() {
-		
+        return this.root == null;
 	}
 
 	clear() {
-		
+		this.root = null;
+		this.parentNodes = [];
 	}
 
 	insertNode(node) {
-		
+        if (this.isEmpty()) {
+            this.root = node;
+            this.parentNodes.push(node);
+        } else {
+            this.parentNodes.push(node);
+            this.parentNodes[0].appendChild(node);
+        }
+        // удаляем узел c двумя детьми
+        if (this.parentNodes[0].left && this.parentNodes[0].right) {
+            this.parentNodes.shift();
+        }
 	}
 
 	shiftNodeUp(node) {
-		
+        if (node.parent) {
+            if (node.priority > node.parent.priority) {
+				// делаем нужные изменения в parentNodes
+				const ni = this.parentNodes.indexOf(node);
+				const pi = this.parentNodes.indexOf(node.parent);
+                if (~ni) {
+                    if (~pi) {
+                        const x = this.parentNodes[ni];
+                        this.parentNodes[ni] = this.parentNodes[pi];
+						this.parentNodes[pi] = x
+					} else {
+                        this.parentNodes[ni] = node.parent;
+					}
+                }
+                node.swapWithParent();
+                this.shiftNodeUp(node);
+            }
+        }
+        else {
+        	this.root = node;
+        }
 	}
 
 	shiftNodeDown(node) {
-		
+
+        if (node && node.left) {
+
+            let up;
+            if (node.right) {
+                if (node.left.priority > node.right.priority) {
+                    up = node.left;
+                } else {
+                    up = node.right;
+                }
+            } else {
+                up = node.left;
+            }
+
+            if (node.priority < up.priority) {
+                const ni = this.parentNodes.indexOf(node);
+                const nc = this.parentNodes.indexOf(up);
+                if (node === this.root) this.root = up;
+                if (~nc) {
+                    if (~ni) {
+                        const x = this.parentNodes[nc];
+                        this.parentNodes[nc] = this.parentNodes[ni];
+                        this.parentNodes[ni] = x;
+                    } else {
+                        this.parentNodes[nc] = node;
+                    }
+                }
+                up.swapWithParent();
+                this.shiftNodeDown(node);
+            }
+        }
 	}
 }
 
--- src/node.js
+++ src/node.js
@@ -1,23 +1,89 @@
 class Node {
 	constructor(data, priority) {
-
+        this.data = data;
+		this.priority = priority;
+		this.parent = null;
+		this.left = null;
+		this.right = null;
 	}
 
 	appendChild(node) {
-
+        if (!this.left) {
+            this.left = node;
+            node.parent = this;
+        } else if (!this.right) {
+            this.right = node;
+            node.parent = this;
+        }
 	}
 
 	removeChild(node) {
-
+		if (this.left != node && this.right != node) {
+            throw Error;
+		}
+        if (this.left == node) {
+        	this.left = null;
+        } else {
+            this.right = null;
+		}
+        node.parent = null;
 	}
 
 	remove() {
-
+        if (this.parent) {
+            this.parent.removeChild(this);
+        }
 	}
 
 	swapWithParent() {
-		
-	}
+        if (this.parent) {
+
+        	// переписываем детей на родителя
+            if (this.left) this.left.parent = this.parent;
+            if (this.right) this.right.parent = this.parent;
+
+            // если this левое
+            if (this === this.parent.left) {
+                if (this.parent.right) this.parent.right.parent = this;
+
+                const r = this.parent.right;
+
+                this.parent.left = this.left;
+				this.parent.right = this.right;
+
+				this.left = this.parent;
+				this.right = r;
+            }
+			// если правое
+            if (this === this.parent.right) {
+                if (this.parent.left) this.parent.left.parent = this;
+
+                const l = this.parent.left;
+
+                this.parent.left = this.left;
+				this.parent.right = this.right;
+
+				this.left = l;
+				this.right = this.parent;
+            }
+
+            // если есть "дедушка" - прописываем себя к нему
+            if (this.parent.parent) {
+                if (this.parent === this.parent.parent.left) {
+                    this.parent.parent.left = this;
+                }
+                if (this.parent === this.parent.parent.right) {
+                    this.parent.parent.right = this;
+                }
+            }
+
+			// меняемся местами с родителем
+            const	x = this.parent.parent;
+            this.parent.parent = this;
+            this.parent = x;
+        }
+    }
+
 }
 
 module.exports = Node;
